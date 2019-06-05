070cf1e7d1dc7c0092edc972657a29f894ea614c
Michael <michael.c.hadley@gmail.com>
2019-06-05 13:47:02 -0400

    init commit

---
+++ js/functions.js
@@ -0,0 +1,27 @@
+$('document').ready
+(function(){
+  getScore();
+  displayResults();
+});
+
+
+function getScore(){
+  var d = new Date();
+  var year = d.getFullYear();
+  var month = String(d.getMonth() + 1).padStart(2, '0');
+  var day = String(d.getDate()).padStart(2, '0');;
+  var url = 'http://gd.mlb.com/components/game/mlb/year_'
+  url += year
+  url += '/month_'
+  url += month
+  url += '/day_'
+  url += day
+  url += '/master_scoreboard.json'
+  
+  $.getJSON(url, displayResults)
+}
+
+function displayResults(dataFromServer){
+  var results = dataFromServer.data.games;
+  
+}
